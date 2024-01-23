f17b980e53fa994a60847a2970d70b900dd65917
Tim <tim@MacBook-Pro-2.local>
2024-01-23 17:10:58 +0300

    small fix in cli.js & add new game brain-even

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [![Actions Status](https://github.com/bdcry/frontend-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/bdcry/frontend-project-44/actions)
 
 [![Maintainability](https://api.codeclimate.com/v1/badges/b125494e2a1ee018c6b8/maintainability)](https://codeclimate.com/github/bdcry/frontend-project-44/maintainability)
+
+[![asciicast](https://asciinema.org/a/5GzKEhEfWemG47bf602uQdTnr.svg)](https://asciinema.org/a/5GzKEhEfWemG47bf602uQdTnr)
---
+++ src/even.js
@@ -0,0 +1,29 @@
+import readlineSync from 'readline-sync';
+import greet from './cli.js';
+
+const isEven = (num) => num % 2 === 0;
+
+const playEvenGame = () => {
+    const userName = greet();
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+    // Проходим три раунда игры
+    const roundsCount = 3;
+
+    for (let i = 0; i < roundsCount; i += 1) {
+        // Рандомное целое число от 0 до 99
+        const question = Math.floor(Math.random() * 100);
+        console.log(`Question: ${question}`);
+        const userAnswer = readlineSync.question('Your answer: ');
+        const correctAnswer = isEven(question) ? 'yes' : 'no';
+
+        if (userAnswer !== correctAnswer) {
+            console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}. \nLet's try again, ${userName}`);
+            return;
+        }
+        console.log('Correct!');
+    }
+    console.log(`Congratulations, ${userName}!`);
+};
+
+export default playEvenGame;
